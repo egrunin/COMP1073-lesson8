28463b5a171c1fab16e5406f809112ea611acce5
Jack <evgeni.grunin@mygeorgian.ca>
2016-03-10 11:02:00 -0800

    fourth commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -5,7 +5,45 @@
 
 "use strict";
 
-var myVariable = "hello";
+var Person = {
+    "age": 40,
+    "name": "Jack",
+    "address": "1 Earth",
+    "city": "Barrie",
+    "province": "Ontario",
+    "familyNames": [
+        "Alex",
+        "Sergei",
+        "Igor",
+        "Dima"
+    ],
+    "sayHello":function(){
+        console.log(Person.name + "  says hello");
+    }
+    
+}; /* var Person = new Object();  */
+
+// Person.age = 40;
+
+// for(var index = 0; index < Person.length; index ++);
+
+for(var key in Person) {
+    console.log(Person[key]);
+    if(key === "familyNames") {
+        console.log("Family Names:  ");
+        console.log("++++++++++++++++++");
+        for(var index=0; index < Person.familyNames.length; index++) {
+            console.log("    " + Person.familyNames[index]);
+        }
+    } else {
+        console.log(Person[key]);
+    }
+}
+
+// console.log(Person.age);
+
+
+/* var myVariable = "hello";
 console.log("myVariable " + myVariable);
 
 // var myArray = new Array();
@@ -19,15 +57,27 @@ var myArray = [
     "Jack G",
     "1 Earth",
     "Barrie",
+    "ontario",
     40,
     true
 ];
 
-console.log(myArray.slice(4,1));
+console.log(myArray.splice(4, 0, "416-797-8527") + "\n");
+console.log('+++++++++++');
+
+myArray[4] = "705-555-5000";
+
+// console.log(myArray.unshift("4167978527") + "\n");
+// console.log('+++++++++++');
+
+// console.log(myArray.slice(4,1));
+
+// console.log(myArray.pop());
+// console.log(myArray.shift());
 
 for(var index=0; index < myArray.length; indexx++) {
     console.log(myArray[index]);
-}
+} */
 
 
 })();
